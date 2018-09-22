bb684bc48e6265bf0c16dfb2e27aba5b9b347eb1
syscheme <syscheme@hotmail.com>
2018-09-22 10:20:21 +0800

    fetcher

--- fetcher/afterMarket_cnA.sh
+++ fetcher/afterMarket_cnA.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 DIR=$(dirname `readlink -f $0`)
 cd $DIR
-python ./TuShareAfterMarket.py 2>&1 > TuShareAfterMarket.log
+ps aux|grep 'python.*TuShareAfterMarket.py'|grep -v grep|awk '{print $2;}'|xargs kill -9
+python ./TuShareAfterMarket.py 2>&1 >> /tmp/TuShareAfterMarket.log &
 
