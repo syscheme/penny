78d664e69eeb0a9248588f7091b24618ec692068
syscheme <syscheme@hotmail.com>
2019-04-19 15:09:38 +0800

    added ETF to collect

--- fetcher/afterMarket_cnA.sh
+++ fetcher/afterMarket_cnA.sh
@@ -2,5 +2,6 @@
 DIR=$(dirname `readlink -f $0`)
 cd $DIR
 ps aux|grep 'python.*TuShareAfterMarket.py'|grep -v grep|awk '{print $2;}'|xargs kill -9
+date +%Y%m%dT%H >> /tmp/TuShareAfterMarket.log
 python ./TuShareAfterMarket.py 2>&1 >> /tmp/TuShareAfterMarket.log &
 
